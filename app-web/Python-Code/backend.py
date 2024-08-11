1a50ec81a9008b84129284b7e0ad156c8b7dfb6b
Juan Diego Campos Quiñones <juandiegoc635@gmail.com>
2024-08-11 00:31:43 -0500

    Avance proyecto reproductor de música

--- app-web/Python-Code/backend.py
+++ app-web/Python-Code/backend.py
@@ -1,24 +1,57 @@
 import pygame
-from tkinter import filedialog
+from tkinter import filedialog, Listbox, END
 
 class MusicPlayer:
-    def __init__(self, label):
+    def __init__(self, label, listbox):
         pygame.mixer.init()
         self.music_label = label
+        self.listbox = listbox
+        self.playlist = []
+
+        # Vincular la selección de una canción en el Listbox con el método play_selected_music
+        self.listbox.bind('<<ListboxSelect>>', self.play_selected_music)
 
     def load_music(self):
-        file = filedialog.askopenfilename(filetypes=[("MP3 files", "*.mp3")])
-        if file:
+        files = filedialog.askopenfilenames(filetypes=[("MP3 files", "*.mp3")])
+        if files:
+            for file in files:
+                self.playlist.append(file)
+                self.listbox.insert(END, file.split("/")[-1])
+
+    def play_selected_music(self, event=None):
+        selected_song_index = self.listbox.curselection()
+        if selected_song_index:
+            file = self.playlist[selected_song_index[0]]
             pygame.mixer.music.load(file)
             pygame.mixer.music.play()
-            self.music_label.config(text="Reproduciendo: " + file.split("/")[-1])
+            self.music_label.config(text="" + file.split("/")[-1])
 
     def play_music(self):
-        pygame.mixer.music.unpause()
-
-    def pause_music(self):
-        pygame.mixer.music.pause()
+        if pygame.mixer.music.get_busy():
+            pygame.mixer.music.pause()
+        else:
+            pygame.mixer.music.unpause()
 
     def stop_music(self):
         pygame.mixer.music.stop()
-        self.music_label.config(text="")
+        self.music_label.config(text="Sin reproducción")
+
+    def next_song(self):
+        current_selection = self.listbox.curselection()
+        if current_selection:
+            next_index = current_selection[0] + 1
+            if next_index < self.listbox.size():
+                self.listbox.selection_clear(0, END)
+                self.listbox.selection_set(next_index)
+                self.listbox.activate(next_index)
+                self.play_selected_music()
+
+    def previous_song(self):
+        current_selection = self.listbox.curselection()
+        if current_selection:
+            prev_index = current_selection[0] - 1
+            if prev_index >= 0:
+                self.listbox.selection_clear(0, END)
+                self.listbox.selection_set(prev_index)
+                self.listbox.activate(prev_index)
+                self.play_selected_music()
--- app-web/Python-Code/reproductor.py
+++ app-web/Python-Code/reproductor.py
@@ -1,6 +1,8 @@
 import tkinter as tk
+from tkinter import Listbox
 import pygame
 from backend import MusicPlayer  # Importa la clase MusicPlayer
+from PIL import Image, ImageTk
 
 # Inicializar pygame mixer (esto también se puede hacer en el __init__ de la clase MusicPlayer)
 pygame.mixer.init()
@@ -8,26 +10,49 @@ pygame.mixer.init()
 # Crear la ventana principal
 root = tk.Tk()
 root.title("Reproductor de Música")
+root.config(bg="#030506")
 
 # Etiqueta para mostrar la canción actual
-music_label = tk.Label(root, text="No hay música reproduciéndose")
-music_label.pack(pady=10)
+music_label = tk.Label(root, text="Sin reproducción", anchor='center', background="#030506", foreground="#fff")
+music_label.grid(row=2, column= 1, columnspan=4)
+
+listbox = Listbox(root, height=30, width=30, background="#030506", highlightbackground="#030506", relief='flat', foreground="#fff")
+listbox.grid(row=1, column=0, rowspan=3)
 
 # Crear una instancia de la clase MusicPlayer y pasar la etiqueta como argumento
-player = MusicPlayer(music_label)
+player = MusicPlayer(music_label, listbox)
 
-# Botones de control de música, vinculando a los métodos de la clase
+
+# Cargar la imagen usando PIL
+image = Image.open(r"C:\Proyecto\Reproductor-de-M-sica\app-web\Python-Code\Images\rigby.jpg")
+# Redimensionar la imagen
+nuevo_tamano = (300, 300)  # Ancho x Alto en píxeles
+image = image.resize(nuevo_tamano, Image.LANCZOS)
+photo = ImageTk.PhotoImage(image)
+#Imagen de reproduccion
+imagen_reproduccion = tk.Label(root, image=photo, highlightbackground="#030506", relief='flat')
+imagen_reproduccion.image = photo  # Esto es necesario para evitar que la imagen sea recolectada por el GC
+imagen_reproduccion.grid(row=1, column=1, columnspan=4, pady=20, padx=20)
+
+# Botones de control de musica, vinculando a los métodos de la clase
 load_button = tk.Button(root, text="Cargar y Reproducir", command=player.load_music)
-load_button.pack(pady=5)
+load_button.grid(row= 0, column= 0, sticky='n')
 
-play_button = tk.Button(root, text="Reanudar", command=player.play_music)
-play_button.pack(pady=5)
+#Boton detener
+stop_button = tk.Button(root, text="⏹︎", command=player.stop_music, width=5, height=2)
+stop_button.grid(row=3, column=1)
 
-pause_button = tk.Button(root, text="Pausar", command=player.pause_music)
-pause_button.pack(pady=5)
+#Boton anterior cancion
+previous_button = tk.Button(root, text="⏮︎", command=player.previous_song, width=5, height=2)
+previous_button.grid(row=3, column=2)
 
-stop_button = tk.Button(root, text="Detener", command=player.stop_music)
-stop_button.pack(pady=5)
+#Boton reproducir/pausar
+play_button = tk.Button(root, text="⏸︎", command=player.play_music, width=5, height=2)
+play_button.grid(row= 3, column= 3)
+
+#Boton siguiente cancion
+next_button = tk.Button(root, text="⏭︎", command=player.next_song, width=5, height=2)
+next_button.grid(row=3, column=4)
 
 # Iniciar el bucle de la interfaz gráfica
 root.mainloop()
